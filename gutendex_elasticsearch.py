4080be8281c4ae90611ea49892b1848df1e93faf
yakalin <ytakalin01@gmail.com>
2024-01-06 22:16:24 +0100

    Chunking and embeddings for every chunk (nested) FINALYYY!!!!!

--- gutendex_elasticsearch.py
+++ gutendex_elasticsearch.py
@@ -1,18 +1,36 @@
 import requests
 from elasticsearch import Elasticsearch
-#import tensorflow as tf
-#from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer, BertTokenizerFast
+import transformers as tf
+import numpy as np
+
+response = requests.get("https://gutendex.com/books/?page=1", headers={"Accept": "application/json"})
+gutendex_array = response.json().get("results")
+model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-small-en', trust_remote_code=True)
+tokenizer = BertTokenizerFast.from_pretrained("jinaai/jina-embeddings-v2-small-en")
+
+
+def split_string_with_limit(text: str, limit: int):
+
+    tokens = tokenizer(text).data['input_ids']
+    token_parts = [tokens[i:i + limit] for i in range(0, len(tokens), limit)]
+    text_parts = []
+
+    for part in token_parts:
+        text_part = [tokenizer.decode(token) for token in part]
+        decoded_text = tokenizer.decode(tokenizer.convert_tokens_to_ids(text_part))
+        text_parts.append(decoded_text)
+    i = 1
+    return text_parts
+
 
 es = Elasticsearch(
-    ['http://localhost:9200'],
+    ['https://localhost:9200'],
     basic_auth=("elastic", "GZLhtJXfckU-DcYQLgYU"),
     verify_certs=False
 )
 print(es.info())
-response = requests.get("https://gutendex.com/books/?page=1", headers={"Accept": "application/json"})
-gutendex_array = response.json().get("results")
-#model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-small-en', trust_remote_code=True)
-#i = 1
+i = 1
 
 for book in gutendex_array:
 
@@ -51,16 +69,21 @@ for book in gutendex_array:
             index_end = text.find("** END")
             text = text[index_start:index_end]
 
-            #text_chunks = [chunk.strip() for chunk in text.split("\r\n\r\n") if chunk.strip()]
-            #print(text)
-            #for chunk in text_chunks:
-                #print(chunk)
-                #chunk = chunk.strip().replace("\n", " ").replace("\r", " ")
-                #embedding = model.encode([chunk])
+            parts = split_string_with_limit(text, 8150)
+            embeddings = []
 
-                #if embedding.shape[1] != 512:
-                    #resize_layer = tf.keras.layers.Dense(512, input_shape=(embedding.shape[1],))
-                    #embedding = resize_layer(embedding)
+            for part in parts:
+                embedding = (model.encode([part]))
+
+                if embedding.shape[1] != 512:
+                    resize_layer = tf.keras.layers.Dense(512, input_shape=(embedding.shape[1],))
+                    embedding = resize_layer(embedding)
+
+                embeddings.append(embedding)
+
+            doc_embeddings = []
+            for emb in embeddings:
+                doc_embeddings.append({"vector": emb[0]})
 
             document = {
                 "author": autorname,
@@ -68,10 +91,10 @@ for book in gutendex_array:
                 "year": year,
                 "language": language,
                 "source": "gutenberg",
-                "text": text
-                #"embedding": [0]
+                "text": text,
+                "embeddings": doc_embeddings
             }
 
-            res = es.index(index="text_index", document=document)
-                #print(f"Dokument {titel} mit ID {i} erstellt.")
-    #i += 1
+            res = es.index(index="texts", document=document)
+            print(f"Dokument {titel} Nummer {i} erstellt.")
+    i += 1
